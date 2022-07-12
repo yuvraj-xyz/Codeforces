69822abb28bb41672d524a383a8cc6e0c6ee98b0
Yuvraj Paliwal <78093228+yuvraj-xyz@users.noreply.github.com>
2022-07-12 10:52:35 -0600

    Create yesorYes.cpp

---
+++ yesorYes.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        string s;
+        cin >> s;
+
+        int count=0;
+        if(s[0]=='Y' || s[0]=='y')
+        {
+            count++;
+        }
+        if(s[1]=='E' || s[1]=='e')
+        {
+            count++;
+        }
+        if(s[2]=='S' || s[2]=='s')
+        {
+            count++;
+        }
+        
+        if(count==3)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+}
