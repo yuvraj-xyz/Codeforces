bb8220463ad433e386a3738a8a6d871e85fcf991
Yuvraj Paliwal <78093228+yuvraj-xyz@users.noreply.github.com>
2022-07-12 09:35:26 -0600

    Create cypher.cpp
    Codeforces Round #806 Division 4
    Cypher c++/cpp Solution Code

---
+++ cypher.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Cypher
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n,temp;
+        cin >> n;
+        vector<int> vec;
+        for(int i=0; i<n; i++)
+        {
+            cin >> temp;
+            vec.push_back(temp);
+        }
+        int temp2 = n;
+        vector<int> res;
+        int i=0;
+        while(temp2--)
+        {
+            int x;
+            string s;
+            cin >> x >> s;
+
+            int a = 0;
+            for(int i=0; i<x; i++)
+            {
+                if(s[i]=='U')
+                {
+                    a--;
+                }
+                else
+                {
+                    a++;
+                }
+            }
+            int ans;
+            ans = vec[i]+ (a);
+            if(ans>9)
+            {
+                ans-=10;
+            }
+            else if(ans<0)
+            {
+                ans+=10;
+            }
+            res.push_back(ans);
+            i++;
+        }
+        for(int i=0; i<res.size(); i++)
+        {
+            cout << res[i] << " ";
+        }
+        cout << endl;
+    }
+}
